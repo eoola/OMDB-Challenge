22f78b21efac011d584bab5d48f5a81e1c2cb999
Emmanuel Ola <54866720+eoola@users.noreply.github.com>
2021-06-16 17:59:14 -0400

    Adds Footer, Movie, MovieList, and Search components

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render() {
+        return(
+            <h5>{this.props.title}</h5>
+        )
+    }
+}
+
+
+export default Footer;
